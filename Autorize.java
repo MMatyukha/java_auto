51100945c9a65d38109495435e9c46c1733b9a49
MMatyukha <77583720+MMatyukha@users.noreply.github.com>
2022-03-27 14:21:05 +0300

    Update Autorize.java

--- Autorize.java
+++ Autorize.java
@@ -4,18 +4,16 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 
 public class Autorize {
-
+    System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
     WebDriver driver = new ChromeDriver();
-   try {
-        driver.get("http://demowebshop.tricentis.com/");
-        driver.findElement(By.cssSelector(".ico-login")).click();
-        driver.findElement(By.xpath("//input[@id='Email']")).sendKeys("m@m.ru");
-        driver.findElement(By.id("Password")).click();
-        driver.findElement(By.xpath("//input[@id='Password']")).sendKeys("19#10mm");
-        driver.findElement(By.cssSelector(".login-button")).click();
-    } (InterruptedException e) {
-        e.printStackTrace();
-    }finally {driver.quit ();
+    driver.get("http://demowebshop.tricentis.com/");
+    driver.findElement(By.cssSelector(".ico-login")).click();
+    driver.findElement(By.xpath("//input[@id='Email']")).sendKeys("m@m.ru");
+    driver.findElement(By.id("Password")).click();
+    driver.findElement(By.xpath("//input[@id='Password']")).sendKeys("19#10mm");
+    driver.findElement(By.cssSelector(".login-button")).click();
+    
+  
    }
 
 }
