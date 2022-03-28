0be59879f58202e6c80c37cd7a0565ab6a0c6390
MMatyukha <77583720+MMatyukha@users.noreply.github.com>
2022-03-28 23:02:35 +0300

    Update Triangle.java

--- less_4/Triangle.java
+++ less_4/Triangle.java
@@ -4,17 +4,30 @@ import java.util.Scanner;
 
 public class Triange {
 
-        public static void main(String args[]) {
-        Scanner scanner = new Scanner(System.in);
+      public static void main(String args[]) {
+                Scanner sc = new Scanner(System.in);
 
-        System.out.println("Введите катет треугольника: ");
-        double base = scanner.nextDouble();
+                System.out.println("Сторона a=");
+                int a = sc.nextInt();
 
-        System.out.println("Введите высоту треугольника: ");
-        double height = scanner.nextDouble();
+                System.out.println("Сторона b=");
+                int b = sc.nextInt();
 
-        //Площадь треугольника вычисляется по формуле = (width*height)/2
-        double area = (base* height)/2;
-        System.out.println("Площадь треугольника равна: " + area);
-    }
-}
+                System.out.println("Сторона c=");
+                int c = sc.nextInt();
+
+                int p = (a + b + c) / 2;
+                double s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+                System.out.println("Площадь треугольника = " + s);
+
+                sc.close();
+                if (a + b < c || a + c < b || b + c < a) {
+                        System.out.println("Эти стороны не образуют треугольника");
+                        System.exit(0);
+                }
+                if (a == (b = c)) {
+                        System.out.println("Эти стороны  образуют треугольника равнобедренный  треугольник");
+                        System.exit(0);
+                }
+        }
